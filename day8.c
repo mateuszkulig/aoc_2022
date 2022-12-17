8cde49fa572d3f00a242cdfb25eac87dc623c9e5
Mateusz Kulig <105542191+mateuszkulig@users.noreply.github.com>
2022-12-17 21:30:50 +0100

    create 2d array dynamically and fill it with data

--- day8.c
+++ day8.c
@@ -8,12 +8,10 @@ int main(int argc, char **argv) {
     FILE    *inputFile;
     int     currentW = 0;
     int     currentH = 0;
-    char    **rows;
-    char    **columns;
+    char    **field;
 
+    // get the dimentions of field
     inputFile = fopen("input8.txt", "r");
-    
-    
     while(fgets(buffer, 128, inputFile) != NULL) {
         for(int i=0; i<128; ++i) {  // this really needs to run only once but runs in loop
             if (buffer[i] != '\0' && buffer[i] != '\n') {
@@ -26,7 +24,29 @@ int main(int argc, char **argv) {
         }
         height++;
     }
+    fclose(inputFile);
 
-    printf("%d\t%d\n", width, height);
+    // allocate memory for field
+    field = malloc(sizeof(char*)*width);
+    for(int i=0; i<height; ++i) {
+        field[i] = malloc(sizeof(char)*height);
+    }
+
+    // fill the rows and columns
+    inputFile = fopen("input8.txt", "r");
+
+    int k=0;
+    while(fgets(buffer, 128, inputFile) != NULL) {
+        printf("%s", buffer);
+        for(int i=0; i<width; ++i) {
+            field[i][k]=buffer[i];
+        }
+        ++k;
+    }
+    fclose(inputFile);
+
+    printf("\n%c", field[0][2]);
+
+    // printf("%d\t%d\n", width, height);
     return 0;
 }
