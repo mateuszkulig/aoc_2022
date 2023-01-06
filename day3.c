699a4020f29a5d5f5786b66a10fdbe46dffef7e5
Mateusz Kulig <105542191+mateuszkulig@users.noreply.github.com>
2023-01-07 00:39:40 +0100

    create findGroupSharingItem func; working puzzle; change input to actual one

--- day3.c
+++ day3.c
@@ -1,7 +1,11 @@
 #include "stdio.h"
+#include "string.h"
 
 // find what char appears in both compartments
 char findSharingItem(char *buffer);
+
+// find what char appears in 3-rucksack group
+char findGroupSharingItem(char *buffer1, char *buffer2, char *buffer3);
 
 // get the length of characters in 128char buffer
 int getBufferLength(char *buffer);
@@ -9,24 +13,50 @@ int getBufferLength(char *buffer);
 int main(int argc, char **argv) {
     FILE    *inputFile;
     char    buffer[128];
+    char    rucksacks[3][128];
     char    repeating;
     int     total = 0;
-    
+    int     counter = 0;
+
     inputFile = fopen("input3.txt", "r");
     
     while (fgets(buffer, 128, inputFile) != 0) {
-        repeating = findSharingItem(buffer);
-        if (repeating < 97) {   // upper letter
-            total += repeating - 65 + 27;   // A ascii code is 65, upper letter priorities start with 27
-        } else {
-            total += repeating - 97 + 1;    // a ascii code is 97, lower letter priorities start with 1
+        strcpy(rucksacks[counter%3], buffer);
+        if (!((counter+1)%3)) {
+            repeating = findGroupSharingItem(rucksacks[0], rucksacks[1], rucksacks[2]); // could probably pass an whole array and divide in func
+            if (repeating < 97) {   // upper letter
+                total += repeating - 65 + 27;   // A ascii code is 65, upper letter priorities start with 27
+            } else {
+                total += repeating - 97 + 1;    // a ascii code is 97, lower letter priorities start with 1
+            }
         }
+        counter++;
+
+        // first part
+        // repeating = findSharingItem(buffer);
+        // if (repeating < 97) {   // upper letter
+        //     total += repeating - 65 + 27;   // A ascii code is 65, upper letter priorities start with 27
+        // } else {
+        //     total += repeating - 97 + 1;    // a ascii code is 97, lower letter priorities start with 1
+        // }
     }
 
     printf("%d\n", total);
     fclose(inputFile);
 
     return 0;
+}
+
+char findGroupSharingItem(char *buffer1, char *buffer2, char *buffer3) {
+    for (int i=0; i<getBufferLength(buffer1); ++i) {
+        for (int j=0; j<getBufferLength(buffer2); ++j) {
+            for (int k=0; k<getBufferLength(buffer3); ++k) {
+                if (buffer1[i] == buffer2[j] && buffer1[i] == buffer3[k] && buffer2[j] == buffer3[k]) {
+                    return buffer1[i];
+                }
+            }
+        }
+    }
 }
 
 char findSharingItem(char *buffer) {
